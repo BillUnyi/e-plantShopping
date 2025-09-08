9ff938ffef67f26e7b323157dc1c5ff9e8f4d24d
Bill Unyi <billunyi@gmail.com>
2025-09-08 16:13:41 -0400

    Updated CartItem

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -19,23 +19,37 @@ const CartItem = ({ onContinueShopping }) => {
     };
 
     const handleContinueShopping = (e) => {
-    
+        onContinueShopping(e);
     };
 
-
+    const handleCheckoutShopping = (e) => {
+        alert('Functionality to be added for future reference');
+    };
 
     const handleIncrement = (item) => {
+        dispatch(updateQuantity({name: item.name, quantity: item.quantity + 1}));
     };
 
     const handleDecrement = (item) => {
-    
+        if (item.quantity > 1) {
+            dispatch(updateQuantity({name: item.name, quantity: item.quantity - 1}));
+        }
+        else {
+            dispatch(removeItem({name: item.name}));
+        }
     };
 
     const handleRemove = (item) => {
+        dispatch(removeItem({name: item.name}));
     };
 
     // Calculate total cost based on quantity for an item
     const calculateTotalCost = (item) => {
+        let total = 0;
+        let cost = parseFloat(item.cost);
+        let quantity = parseFloat(item.quantity);
+        total += cost * quantity;
+        return total;
     };
 
     return (
